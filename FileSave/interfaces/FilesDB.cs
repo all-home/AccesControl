c1277dca239e6f121137415fde1ac7d01c682516
all-home <mrp99999999@gmail.com>
2020-08-30 20:21:54 +0300

    Add Files Intefaces

---
+++ FileSave/interfaces/FilesDB.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FileSave.Models;
+
+namespace FileSave.interfaces
+{
+    interface FilesDB
+    {
+        IEnumerable<File> Get();
+        void Get(int id);
+        void Create(File file);
+        void Delete(int id);
+         
+
+
+    }
+}
