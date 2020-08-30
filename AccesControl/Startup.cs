f7de3f26ff4b3f75e5e6d773a863d8be6e242524
all-home <mrp99999999@gmail.com>
2020-08-30 18:26:06 +0300

    add default endpoint

--- AccesControl/Startup.cs
+++ AccesControl/Startup.cs
@@ -60,7 +60,9 @@ namespace AccesControl
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapRazorPages();
+                endpoints.MapControllerRoute(
+               name: "default",
+               pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
