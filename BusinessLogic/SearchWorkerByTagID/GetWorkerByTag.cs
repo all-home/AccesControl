26c09cd63a9a3969a0e6ddea58c1537c42e96c1c
all-home <mrp99999999@gmail.com>
2020-09-08 20:44:44 +0300

    fix interface IStat

---
+++ BusinessLogic/SearchWorkerByTagID/GetWorkerByTag.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WorkersDB.Interfaces;
+using ProfilesDB.Interfaces;
+
+namespace BusinessLogic.SearchWorkerByTagID
+{
+    class GetWorkerByTag
+    {
+        public GetWorkerByTag(IStat )
+        {
+        }
+    }
+}
