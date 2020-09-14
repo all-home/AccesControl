ae03e2c372e1622843c9e48aaa128b97a80f397b
all-home <mrp99999999@gmail.com>
2020-09-14 23:57:48 +0300

    add stat model and code for dispaly stat

--- AccesControl/Controllers/StatController.cs
+++ AccesControl/Controllers/StatController.cs
@@ -5,21 +5,54 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using WorkersDB.Interfaces;
 using WorkersDB.Models;
+using AccesControl.ViewModels;
+using System.Data.SqlTypes;
+using BusinessLogic.Interfaces;
 
 namespace AccesControl.Controllers
 {
     public class StatController : Controller
     {
         IStat stat;
+        IWorkersRepo workersRepo;
 
-        public StatController(IStat _stat)
+        public StatController(IStat _stat, IWorkersRepo _Repo)
         {
-            stat = _stat;        
+            stat = _stat;
+            workersRepo = _Repo;
         }
         public IActionResult Index()
         {
+            var _stat = stat.Get();
+            List<StatModel> statModels = new List<StatModel>();
+                            
+                foreach (Statistics a in _stat)
+                {
+                    Worker cWorker = workersRepo.Get(a.WorkerID);
+
+                statModels.Add(new StatModel
+                {
+                    WorkerID = cWorker.id,
+                    Name = cWorker.Name,
+                    Surname = cWorker.Surname,
+                    Patronymic = cWorker.Patronymic,
+                    StartWork = a.StartWork,
+                    EndWork = a.EndWork,
+                    Late = a.Late,
+                    Latetime = a.Latetime
+
+                }); ;
+                }
+
+            if (statModels != null)
+            {
+                return View(statModels);
+            }
+            else
+            {
+                return View();
+            }
             
-            return View(stat.Get());
         }
     }
 }
---
+++ AccesControl/ViewModels/StatModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AccesControl.ViewModels
+{
+    public class StatModel
+    {
+        public DateTime StartWork { get; set; }
+        public DateTime EndWork { get; set; }
+        public string Latetime { get; set; }
+        public bool Late { get; set; }
+        public int WorkerID { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Patronymic { get; set; }
+    }
+}
